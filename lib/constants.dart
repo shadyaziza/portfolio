253a57dffa14d6958c0402f25cdd91ae1beabae1
shadyaziza <shadyaziza@gmail.com>
2021-04-21 01:14:34 +0200

    Portfolio initial trial

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+final kPrimaryColor = Color(0xff011C44);
+final kSecondaryColor = Color(0xff6D004F);
+final kCanvasColor = Color(0xffFFB800);
+final kScaffoldColor = Color(0xffE9E9E9);
